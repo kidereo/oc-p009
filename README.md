47282ad996cab9f6b9d2e1459bff74fb9069e46d
kidereo <kirill.derevenski@gmail.com>
2022-07-19 11:28:50 +0200

    Fixed bug 3.

--- README.md
+++ README.md
@@ -5,8 +5,13 @@
 Développeur d'application - JavaScript React
 
 ## Bug / Fix summary 
+
+[See project kanban "Billed bugs and tests TO DO"](https://www.notion.so/a7a612fc166747e78d95aa38106a55ec?v=2a8d3553379c4366b6f66490ab8f0b90)
+
 1. Bug report - Bills: Missing sorting function. Fix: Added `orderBillsByDate()` in `./src/containers/Bills.js` to apply to data in `./src/views/BillsUI.js`.
 2. Bug report - Login: Erroneous copy/paste of `e.target.querySelector()` inside `handleSubmitAdmin()` function. Fix: Changed selectors to `input[data-testid="admin-email-input"]` and `input[data-testid="admin-password-input"]`. 
+3. Bug Hunt - Bills: Missing file extension validation. Fix: Added `if() else` inspection inside `handleChangeFile()` function in `./src/containers/NewBill.js`.
+
 ## Comment lancer l'application en local ?
 
 ### étape 1 - Lancer le backend :
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,76 +1,90 @@
-import { ROUTES_PATH } from '../constants/routes.js'
+import {ROUTES_PATH} from '../constants/routes.js'
 import Logout from "./Logout.js"
 
 export default class NewBill {
-  constructor({ document, onNavigate, store, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.store = store
-    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
-    formNewBill.addEventListener("submit", this.handleSubmit)
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-    file.addEventListener("change", this.handleChangeFile)
-    this.fileUrl = null
-    this.fileName = null
-    this.billId = null
-    new Logout({ document, localStorage, onNavigate })
-  }
-  handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    constructor({document, onNavigate, store, localStorage}) {
+        this.document = document
+        this.onNavigate = onNavigate
+        this.store = store
+        const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+        formNewBill.addEventListener("submit", this.handleSubmit)
+        const file = this.document.querySelector(`input[data-testid="file"]`)
+        file.addEventListener("change", this.handleChangeFile)
+        this.fileUrl = null
+        this.fileName = null
+        this.billId = null
+        new Logout({document, localStorage, onNavigate})
+    }
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
+    handleChangeFile = e => {
+        e.preventDefault()
+        const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+        const filePath = e.target.value.split(/\\/g)
+        const fileName = filePath[filePath.length - 1]
+        const formData = new FormData()
+        const email = JSON.parse(localStorage.getItem("user")).email
+        formData.append('file', file)
+        formData.append('email', email)
+
+        /**
+         * Add file extension validation and submit only those with approved extensions.
+         * 1. Inspect extension and submit if valid.
+         * 2. If the extension is not valid, clear the file input and show alert.
+         */
+        let fileExtension = fileName.split(".").pop().toUpperCase();
+        if (fileExtension === "PNG" || fileExtension === "JPG" || fileExtension === "JPEG") {
+            this.store
+                .bills()
+                .create({
+                    data: formData,
+                    headers: {
+                        noContentType: true
+                    }
+                })
+                .then(({fileUrl, key}) => {
+                    console.log(fileUrl)
+                    this.billId = key
+                    this.fileUrl = fileUrl
+                    this.fileName = fileName
+                }).catch(error => console.error(error))
+        } else {
+            e.target.value = "";
+            alert("You have attached a file with a wrong file extension. Only JPEG, JPG and PNG files are accepted. Your claim will not be submitted.");
         }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
-  }
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    const email = JSON.parse(localStorage.getItem("user")).email
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending'
-    }
-    this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
-  }
 
-  // not need to cover this function by tests
-  updateBill = (bill) => {
-    if (this.store) {
-      this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
     }
-  }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+        const email = JSON.parse(localStorage.getItem("user")).email
+        const bill = {
+            email,
+            type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+            name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+            amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+            date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+            vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+            pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+            commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+            fileUrl: this.fileUrl,
+            fileName: this.fileName,
+            status: 'pending'
+        }
+        this.updateBill(bill)
+        this.onNavigate(ROUTES_PATH['Bills'])
+    }
+
+    // not need to cover this function by tests
+    updateBill = (bill) => {
+        if (this.store) {
+            this.store
+                .bills()
+                .update({data: JSON.stringify(bill), selector: this.billId})
+                .then(() => {
+                    this.onNavigate(ROUTES_PATH['Bills'])
+                })
+                .catch(error => console.error(error))
+        }
+    }
 }
